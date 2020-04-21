92769b544471ecdc1ab3a84a4839071cee8e9de7
jxyyl-0327 <2574570725@qq.com>
2020-04-21 22:07:07 +0800

    限制540大小

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="page-home">
-    <h1>Home 首页</h1>
+    <header class="index-header">
+      <a href="mine.html?cpid=0">
+        <div class="header-user">
+          <div class="user-btn"></div>
+        </div>
+      </a>
+      <div class="header-logo"></div>
+      <a href="search.html?cpid=0">
+        <div class="header-search"></div>
+      </a>
+    </header>
   </div>
 </template>
 <script>
@@ -8,3 +18,38 @@ export default {
   name: 'Home'
 }
 </script>
+
+<style lang="scss" scoped>
+.page-home {
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+
+  .index-header {
+    height: 44px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+    box-sizing: border-box;
+    .user-btn {
+      width: 25px;
+      height: 25px;
+      background: url("../../assets/icon/user-btn.png") no-repeat;
+      background-size: 100%;
+    }
+    .header-logo {
+      width: 92px;
+      height: 28px;
+      background: url("../../assets/icon/logo.png") no-repeat;
+      background-size: 100%;
+    }
+    .header-search {
+      width: 25px;
+      height: 25px;
+      background: url("../../assets/icon/search.png") no-repeat;
+      background-size: 100%;
+    }
+  }
+}
+</style>
